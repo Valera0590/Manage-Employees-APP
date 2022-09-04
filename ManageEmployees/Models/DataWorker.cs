500428e1e9d9cd59f2a4a8fdc6353a622e179530
Valeriy <valerka.kondratenkov@mail.ru>
2022-09-04 19:56:42 +0500

    Создание заготовок основных классов

---
+++ ManageEmployees/Models/DataWorker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageEmployees.Models
+{
+    public static class DataWorker
+    {
+        //создание отдела
+
+        //создание позиции
+
+        //создание сотрудника
+
+        //редактирование отдела
+
+        //редактирование позиции
+
+        //редактирование сотрудника
+
+        //удаление отдела
+
+        //удаление позиции
+
+        //удаление сотрудника
+
+    }
+}
